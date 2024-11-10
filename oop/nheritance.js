97cfed868e757559c68623bab9965a7f31538418
dika ramadani <dikabangkok21@gmail.com>
2024-11-10 23:12:25 +0700

    new nokkia

--- oop/nheritance.js
+++ oop/nheritance.js
@@ -28,15 +28,25 @@ class SmartPhones {
           console.log('windows have a bios')
       }
   }
+
+  class nokia  extends SmartPhones {
+    metalBody() {
+      console.log('Nokia have a metal body');
+    }
+  }
   
   const ios = new iOS('black', 'A', '12 Pro Max', 1000);
   const android = new Android('white', 'B', 'Galaxy S21', 3000);
   const windows = new Windows('black', 'i8', 'fujitsu', 5000);
+  const Nokia = new nokia('black', 'i8', 'bapak', 5000);
   
   ios.charging(); // Output: Charging 12 Pro Max
   ios.airDrop(); // Output: iOS have a behavior AirDrop
   windows.bios
+  Nokia.metalBody();
+  Nokia.charging();
   
   android.charging(); // Output: Charging Galaxy S21
   android.splitScreen(); // Output: Android have a Split Screen
   windows.bios();
+  Nokia.metalBody();
