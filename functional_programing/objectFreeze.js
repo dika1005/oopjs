d870878a0a67cab4591443ea5060d1ad9a066b65
dika ramadani <dikabangkok21@gmail.com>
2024-11-29 23:16:49 +0700

    end

---
+++ functional_programing/objectFreeze.js
@@ -0,0 +1,29 @@
+function deepFreeze(object) {
+    Object.keys(object).forEach((name) => {
+      const prop = object[name];
+      if (typeof prop == 'object' && prop !== null) {
+        deepFreeze(prop);
+      }
+    });
+  
+    return Object.freeze(object);
+  }
+  
+  const complexUser = {
+    name: 'Bob',
+    email: 'bob@dicoding.com',
+    preferences: {
+      newsletter: true,
+      notifications: 'weekly',
+      address: {
+        city: 'New York',
+        zip: '10001'
+      }
+    }
+  };
+  
+  deepFreeze(complexUser);
+
+  complexUser.preferences.address.city = 'Los Angeles';
+  
+  console.log(complexUser.preferences.address.city); 
