3c703e62a66984f9401c11d85778cb491c0f538c
dika ramadani <dikabangkok21@gmail.com>
2024-12-04 16:59:06 +0700

    await

--- Asyncronus/await/main.mjs
+++ Asyncronus/await/main.mjs
@@ -1,14 +1,14 @@
-import { doSomething } from './a1.mjs';
+import { doSomething } from "./a1.mjs";
 
 async function promiseWithAsyncAwait() {
-    try {
-        console.log('start');
-        const result = await doSomething();
-        console.log(result);
-        console.log('end');
-    } catch (error) {
-        console.log(error.message);
-    }
-};
+  try {
+    console.log("start");
+    const result = await doSomething();
+    console.log(result);
+    console.log("end");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 promiseWithAsyncAwait();
